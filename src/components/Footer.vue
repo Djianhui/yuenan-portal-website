96f5e3250ae37ca009074b1eae6545803da8008a
duanjianhui <1547554061@qq.com>
2025-12-05 18:18:48 +0800

    提交代码

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-  <footer class="w-full h-[669px] relative overflow-hidden">
+  <footer class="w-full h-[69px] relative overflow-hidden">
     <img src="../img/footer.png" alt="城市夜景" class="absolute inset-0 w-full h-full object-cover">
     <div class="absolute inset-0 bg-black bg-opacity-50"></div>
     <div class="relative z-10 h-full flex items-center justify-center">
-      <h2 class="text-2xl font-bold text-white" style="font-family: 'KaiTi', serif;">© 2025 {{ t('common.fullCompany') }} 版权所有</h2>
+      <h2 class="text-2xl font-bold text-white" style="font-family: 'KaiTi', serif;">© 2025 {{ t('common.fullCompany') }}</h2>
     </div>
   </footer>
 </template>
